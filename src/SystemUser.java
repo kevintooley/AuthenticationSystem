881c62b6067ae64a0d38bc12d74c580ee7d2bf07
Kevin Tooley <kevin.tooley@snhu.edu>
2019-03-10 21:24:10 -0400

    Initial Commit
    This is the inital commit of the authentication system.

---
+++ src/SystemUser.java
@@ -0,0 +1,52 @@
+package zooauthenticationsystem;
+
+/**
+ *This public class holds the userName and role of authenticated users
+ * @author Kevin Tooley
+ */
+public class SystemUser {
+    
+    //public enum UserRole {ZOOKEEPER, VETERINARIAN, ADMIN}
+    
+    private static String userName;
+    private static String myRole;
+    
+    public SystemUser () {
+        
+        userName = "none";
+        myRole = "none";
+        
+    }
+    
+    /**
+     * Mutator method to set the private member userName
+     * @param thisUserName pass in userName
+     */
+    public static void setUserName(String thisUserName) {
+        userName = thisUserName;
+    }
+    
+    /**
+     * Accessor method to get the private userName
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+    
+    /**
+     * Mutator method to set the private myRole
+     * @param thisRole pass in user role
+     */
+    public void setMyRole(String thisRole) {
+        myRole = thisRole;
+    }
+    
+    /**
+     * Accessor method to get myRole
+     * @return myRole
+     */
+    public String getMyRole() {
+        return myRole;
+    }
+}
---
+++ src/ZooAuthenticationSystem.java
@@ -0,0 +1,280 @@
+package zooauthenticationsystem;
+
+import java.io.BufferedReader;
+import java.security.MessageDigest;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.NoSuchElementException;
+
+/**
+ * This system will authenticate users.  After authenticated, the user will be given
+ * authority to perform certain actions based on their assigned role.
+ * @author Kevin Tooley
+ */
+public class ZooAuthenticationSystem {
+
+    static SystemUser user = new SystemUser();  //Initialize SystemUser class
+    static final String USER_DIR = System.getProperty("user.dir");  //Directory path for support files
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scnr = new Scanner(System.in);  //System.in scanner
+
+        String userName = "";  //Holds the scanner input
+        String userPassword = "";  //Holds the scanner input
+        
+        boolean remainOnline = true;  //Setting to false will log off user
+            
+        //System.out.println("Authenticate User: " + authenticateUser(scnr, userName, userPassword));
+        //System.out.println("Directory: " + USER_DIR);
+    
+        if (authenticateUser(scnr, userName, userPassword)) {  //If authentication is successful
+            
+            //System.out.println("User Name: " + user.getUserName());
+            //System.out.println("User Role: " + user.getMyRole());
+            
+            switch (user.getMyRole()) {  //get role information based on credentials
+                case "admin": 
+                    getRoleFile("admin");
+                    break;
+                case "zookeeper":
+                    getRoleFile("zookeeper");
+                    break;
+                case "veterinarian":
+                    getRoleFile("veterinarian");
+                    break;
+                default:
+                    System.out.println("Please contact the system administrator to get the proper permissions.");
+                    System.exit(0);
+                
+            }
+            
+            while (remainOnline) { //Menu Loops while user selects anything other than q (quit)
+                String prompt = "";
+                System.out.println("*** SYSTEM MENU ***");
+                System.out.println("1 - Perform Task A");
+                System.out.println("2 - Perform Task B");
+                System.out.println("q - Quit");
+                System.out.println("Select and option: ");
+                prompt = scnr.nextLine();
+                switch (prompt) {
+                    case "1":
+                    case "2":  //Both Case 1 and 2 are stubbed out for future development
+                        System.out.println("");
+                        System.out.println("TODO - Finish Tasks");
+                        System.out.println("");
+                        break;
+                    case "q":
+                        remainOnline = false;
+                        System.out.println("");
+                        System.out.println("Quit.  Goodbye.");
+                        break;
+                    default:
+                        System.out.println("");
+                        System.out.println("Please enter a valid option...");
+                }
+            }
+            
+        }
+        
+    }
+    
+    /**
+     * Users must login to the system, and this method provides the user authentication.  
+     * This function calls validateCredentials().
+     * @param thisScanner Pass scnr from main class
+     * @param userName pass userName from main class
+     * @param userPassword pass userPassword from main class
+     * @return passwordMatch
+     * @see verifyCredentials
+     */
+    public static boolean authenticateUser(Scanner thisScanner, String userName, String userPassword) {
+        
+        //boolean userNameMatch = false;
+        boolean passwordMatch = false;  //set to true when user is authenticated
+        int loginAttempts = 0;  //Counts the number of login attempts
+        
+        while (!passwordMatch) { //While not authenticated...
+            if (loginAttempts >= 3) {  //Only allow 3 login attempts
+                System.out.println("Too many login attempts.  Goodbye.");
+                System.exit(0);
+            }
+            else {
+                System.out.print("User Name: ");
+                userName = thisScanner.next();  //next() vice nextLine(): no spaces in username
+                
+                thisScanner.nextLine(); //consume the hanging /n
+                
+                System.out.print("User Password: ");
+                userPassword = thisScanner.nextLine();
+                
+                //Convert the password to MD5 hash for evaluation against Credentials File
+                try {
+                    String original = userPassword;
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    md.update(original.getBytes());
+                    byte[] digest = md.digest();
+                    StringBuffer sb = new StringBuffer();
+                    for (byte b : digest) {
+                            sb.append(String.format("%02x", b & 0xff));
+                    }
+                    //DEGUG prints
+                    //System.out.println("original:" + original);
+                    //System.out.println("digested:" + sb.toString()); //sb.toString() is what you'll need to compare password strings
+
+                    if (verifyCredentials(userName, sb.toString())) {  //if userName and password(hash) are valid
+                        
+                        passwordMatch = true;
+                        
+                        break;
+                    }
+                    
+                    else {  //triggered if userName is valid but password fails
+                        loginAttempts++;
+                        System.out.println("Invalid credentials...");
+                        System.out.println("");
+                    }
+                    
+                } catch (NoSuchElementException nElement) {  //triggered if userName is invalid
+                    loginAttempts++;
+                    System.out.println("Invalid credentials...");
+                    System.out.println("");
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+                
+            } 
+        }
+        
+        return passwordMatch;
+    }
+    
+    /**
+     * This function verifies that the user entered data matches the credentials on file.
+     * @param thisUserName userName string passed from authenticateUser
+     * @param thisUserPassword  userPassword string passed from authenticateUser
+     * @return boolean isValid
+     * @see authenticateUser
+     */
+    public static boolean verifyCredentials(String thisUserName, String thisUserPassword) {
+        
+        boolean foundUserName = false;  //Condition for while loop
+        boolean isValid = false;  //Return value; true if validated credentials
+        String[] userCredentials = new String[4];  //holds credentials of validated userName
+        //final String USER_DIR = System.getProperty("user.dir");  //Directory path for support files
+        
+        //System.out.println("USER_DIR: " + USER_DIR);
+        
+        FileInputStream fileByteStream = null; // Initialize file input stream
+        Scanner inFS = null;  //Initialize an input file stream scanner
+        
+        //System.out.println("Opening file...");
+        try {
+
+            //fileByteStream = new FileInputStream(USER_DIR + "\\src\\zooauthenticationsystem\\CredentialsFile");
+            /*if (USER_DIR.contains("/")) {
+                //fileByteStream = new FileInputStream(USER_DIR + "/src/zooauthenticationsystem/CredentialsFile");
+                //fileByteStream = new FileInputStream(USER_DIR + "/CredentialsFile");
+                fileByteStream = new FileInputStream("CredentialsFile");
+            }
+            else {
+                //fileByteStream = new FileInputStream(USER_DIR + "\\src\\zooauthenticationsystem\\CredentialsFile");
+                fileByteStream = new FileInputStream(USER_DIR + "\\CredentialsFile");
+            }*/
+            fileByteStream = new FileInputStream("CredentialsFile");
+            inFS = new Scanner(fileByteStream);
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex);
+            
+        }
+        
+        //System.out.println("Parsing file...");
+        while (!foundUserName) {  //Loop through the credential file and search for userName
+            
+            String currentLine = "";  //Initialize a temporary string for the scanner
+            currentLine = inFS.nextLine();  //copy the scanner input
+            //System.out.println("currentLine: " + currentLine);  
+            
+            if (currentLine.contains(thisUserName)) { //if userName is in credential file
+                
+                foundUserName = true;
+                //System.out.println("FOUND THE USER");
+                //System.out.println("currentLine: " + currentLine);  
+                
+                if (currentLine.contains(thisUserPassword)) {  //if password(hash) is correct for valid userName
+                    
+                    isValid = true;
+                    userCredentials = currentLine.split(";");  //credential file delimiter
+                    user.setUserName(thisUserName);
+                    user.setMyRole(userCredentials[3]);
+                    System.out.println("");
+                    
+                    /*
+                    int i;
+                    for (i = 0; i < userCredentials.length; i++) {
+                        System.out.println("userCredentials Array: " + userCredentials[i]);
+
+                    }
+                    */
+                }
+                
+            }
+        }
+        
+        //System.out.println("Closing file...");
+        try {
+            
+            fileByteStream.close();  //Close the credential file
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex);
+            
+        }
+
+        return isValid;  //user is authenticated
+    }
+    
+    /**
+     * This function initializes a BufferedReader (reader) and reads each line of the file.  
+     * @param myRoleFile Pass in role file to parse
+     */
+    public static void getRoleFile(String myRoleFile) {
+        
+        //Initialize bufferedReader for the file
+        try {
+            //BufferedReader reader = new BufferedReader(new FileReader(USER_DIR + "/src/zooauthenticationsystem/" + myRoleFile));
+            /*if (USER_DIR.contains("/")) {
+                //BufferedReader reader = new BufferedReader(new FileReader(USER_DIR + "/" + myRoleFile));
+                BufferedReader reader = new BufferedReader(new FileReader(myRoleFile));
+                String line = null;
+                while ((line = reader.readLine()) != null) { //Loop until end of file
+                    System.out.println(line);
+                }
+            }
+            else {
+                BufferedReader reader = new BufferedReader(new FileReader(USER_DIR + "\\" + myRoleFile));
+                String line = null;
+                while ((line = reader.readLine()) != null) { //Loop until end of file
+                    System.out.println(line);
+                }
+            }*/
+            BufferedReader reader = new BufferedReader(new FileReader(myRoleFile));
+            String line = null;
+            while ((line = reader.readLine()) != null) { //Loop until end of file
+                System.out.println(line);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        System.out.println("");
+        
+    }
+
+}
